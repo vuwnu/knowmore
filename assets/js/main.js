40ad54103a2e6eaa13acd4578d6328fa12960d66
viri.space <admin@viri.xyz>
2019-12-19 16:23:56 +0000

    fixed filters

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,10 +64,10 @@ function sortTable(n) {
 }
 
 // Filter table
-function filterTable() {
+function filterTitle() {
   // Declare variables
   var input, filter, table, tr, td, i, txtValue;
-  input = document.getElementById("filterInput");
+  input = document.getElementById("filterTitle");
   filter = input.value.toUpperCase();
   table = document.getElementById("projectTable");
   tr = table.getElementsByTagName("tr");
@@ -75,6 +75,29 @@ function filterTable() {
   // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
     td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+// Filter table
+function filterTags() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("filterTags");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("projectTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
     if (td) {
       txtValue = td.textContent || td.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
--- p/linkdump.md
+++ p/linkdump.md
@@ -8,23 +8,22 @@ tags: [list]
 This is a collection of links that I find worth reading. I've divided them into sections based on topic. Might integrate these links into other pages once I have stuff properly setup.
 
 Unity
----------
+---
 
   * [WeaverDev Blog](https://www.weaverdev.io/)
   * [Brackeys](https://www.youtube.com/channel/UCYbK_tjZ2OrIZFBvU6CCMiA)
 
 Blender
----------
-
+---
   * [Lazy Blender Tutorials](https://www.youtube.com/channel/UCbmxZRQk-X0p-TOxd6PEYJA)
   * [CGMatter](https://www.youtube.com/channel/UCy1f4m64dwCwk8CBZ_vHfPg)
 
 Javascript
----------
+---
 
   * [Javascript Kit](http://www.javascriptkit.com/)
 
 CSS
----------
+---
 
   * [Creating a CSS Framework](https://www.codementor.io/ismail16smakosh23/my-short-journey-building-unnamed-my-own-css-framework-csqi9o679)
