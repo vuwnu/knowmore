2bd0032c4648a33c5a02cc70398011b8d4d5f390
viri.space <admin@viri.xyz>
2020-09-15 21:02:24 +0100

    removed old script

--- scripts.js
+++ scripts.js
@@ -46,41 +46,6 @@ class KMTable extends HTMLElement {
 
 customElements.define("km-table", KMTable);
 
-
-function json2Table(json) {
-  let cols = Object.keys(json[0]);
-
-  //Map over columns, make headers,join into string
-  let headerRow = cols
-    .map(col => `<th>${col}</th>`)
-    .join("");
-
-  //map over array of json objs, for each row(obj) map over column values,
-  //and return a td with the value of that object for its column
-  //take that array of tds and join them
-  //then return a row of the tds
-  //finally join all the rows together
-  let rows = json
-    .map(row => {
-      let tds = cols.map(col => `<td>${row[col]}</td>`).join("");
-      return `<tr>${tds}</tr>`;
-    })
-    .join("");
-
-  //build the table
-  const table = `
-	<table>
-		<thead>
-			<tr>${headerRow}</tr>
-		<thead>
-		<tbody>
-			${rows}
-		<tbody>
-	<table>`;
-
-  return table;
-}
-
 data =[
   {
     "name": "customise-spotify-with-spicetify",
@@ -106,8 +71,3 @@ data =[
     ]
   }
 ]
-
-setTimeout(function () {
-  output = document.getElementById('output')
-  output.innerHTML = json2Table(data)
-}, 5000);
