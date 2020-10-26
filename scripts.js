275c5f7f820c3ce89abaa2db1f76a485a7b538b0
viri.space <admin@viri.xyz>
2020-10-26 12:14:44 +0000

    updated script

--- scripts.js
+++ scripts.js
@@ -1,25 +1,39 @@
 data =[
   {
-    "name": '<a href="/p/modify-spotify-with-spicetify">modify-spotify-with-spicetify</a>',
+    "name": '<a href="/p/modify-spotify-with-spicetify">Modify Spotify with Spicetify</a>',
     "tags": [
       "spotify",
-      "cusomisation",
+      "customisation",
       "cli"
     ]
   },
   {
-    "name": '<a href="/p/fl-studio-tips">fl-studio-tips</a>',
+    "name": '<a href="/p/fl-studio-tips">FL Studio Tips</a>',
     "tags": [
       "fl-studio",
       "music"
     ]
   },
   {
-    "name": '<a href="/p/blender-addons">blender-addons</a>',
+    "name": '<a href="/p/blender-addons">Blender Addons</a>',
     "tags": [
       "blender",
       "addons",
       "3d-modelling"
+    ]
+  },
+  {
+    "name": '<a href="/p/3d-modeling-links">3D Modeling Links</a>',
+    "tags": [
+      "blender",
+      "3d-modelling"
+    ]
+  },
+  {
+    "name": '<a href="/p/useful-webdev-tools">Useful Webdev Tools</a>',
+    "tags": [
+      "webdev",
+      "tools",
     ]
   }
 ]
